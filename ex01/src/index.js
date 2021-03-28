b55ce224855d8387d13f17e895522e80f3a6682d
aharis <hare.arapovic@gmail.com>
2021-03-29 01:34:24 +0200

    dodao zadatak Day20-21

---
+++ ex01/src/index.js
@@ -0,0 +1,12 @@
+let keys = document.querySelectorAll('.keyboard_wrapper>.key .row span'),
+keyPad = document.querySelector('.keyboard_wrapper>.key'),
+display = document.querySelector('.keyboard_wrapper>.display');
+
+if(keys && keyPad && display) {
+
+    keys.forEach(key=>{
+        key.addEventListener('click',function(){
+            
+        })
+    });
+}
